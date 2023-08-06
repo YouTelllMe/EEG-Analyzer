ef0b60022898a1df90f4863ade06c5a9fac9033c
Marcus Lai <marcus41881653@gmail.com>
2023-08-05 23:33:35 -0700

    added flow drift filtering, epoching, and plotting functionality

--- load.py
+++ load.py
@@ -1,17 +1,49 @@
 import mne
 import os
 
+
 def load_mff(path: str) -> mne.io.Raw:
     """
-    Read mff EEG data into a Raw Object
-
-    mne.io.Raw attributes:
-        [obj].info(): overview info
-        [obj].ch_names(): channel names
-        [obj].n_times(): number of time points
-        ... for more consult https://mne.tools/stable/generated/mne.io.Raw.html#mne.io.Raw
+    Load EEG data in the .mff format into a mne.io.Raw object
     """
     assert os.path.isdir(path), f"{path} does not exist"
     raw_mff = mne.io.read_raw_egi(path)
     return raw_mff
 
+
+def frequency_filter(raw: mne.io.Raw,
+                     lowpass: int | None = None,
+                     highpass: int | None = None
+                     ) -> mne.io.Raw:
+    """
+    Filters MNE Raw data object by frequency.
+
+    Params
+    ======
+    raw: Raw MNE data object
+    lowpass: lower boundary for frequency
+    highpass: higher boundary for frequency
+    """
+    raw.load_data()
+    return raw.copy().filter(l_freq = lowpass, h_freq = highpass)
+
+
+def epoch(raw: mne.io.Raw, 
+             stim_channel: str = "STI 014", 
+             events_key: dict[str, int] = None
+             ) -> mne.Epochs:
+    """
+    Performs epoching on data based on stimulus events.
+
+    Params
+    ======
+    raw: Raw MNE data object
+    """
+    events = mne.find_events(raw, stim_channel=stim_channel)
+    epochs = mne.Epochs(raw, events, event_id = events_key)
+    return epochs
+
+
+
+def artifact_filtering():
+    pass
---
+++ plot.py
@@ -0,0 +1,42 @@
+import mne
+import matplotlib.pyplot as plt
+import os
+
+
+def plot_epoch(epochs: mne.Epochs) -> None:
+    epochs.plot(n_epochs=10)
+    plt.show()
+
+def plot_interactive_all(raw: mne.io.Raw, 
+                         duration: int = 60,
+                         scalings: float | None = None, 
+                         channels: list[str] | None = None,
+                         n_channels: int = 20,
+                         lowpass: float | None = None, 
+                         highpass: float | None = None, 
+                        ) -> None:
+    """
+    Plots an interactive windows of an mne.io.Raw object.
+
+    Param
+    =====
+    raw: Raw MNE data object
+    duration: width of interactive window 
+    scalings: amplitude scaling, increase to fit data into the plot
+    channels: channels to display
+    n_channels: the number of channels to display; height of interactive window
+    lowpass: lower boundary for frequency
+    highpass: higher boundary for frequency
+    """
+    raw = raw.pick(channels)
+    raw.plot(duration=duration, 
+             proj=False, 
+             scalings=scalings, 
+             lowpass=lowpass,
+             highpass=highpass, 
+             n_channels=min(len(raw.ch_names),n_channels))
+    plt.show()
+
+
+def plot_spectrum():
+    pass
